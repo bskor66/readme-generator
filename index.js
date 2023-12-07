d2ba4539454f7f0a8c19cdf612be7a54268bddc0
Blake Skor <blake.skor@outlook.com>
2023-12-07 17:45:05 -0500

    test output with title and desc, added async append methods

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-const outputName = "README.md";
-const outputDir = path.join(__dirname, "Output", outputName);
+// THEN a high-quality, professional README.md is generated with the  and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-console.log(outputDir);
-
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 // WHEN I choose a license for my application from a list of options
@@ -20,4 +14,47 @@ console.log(outputDir);
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
-// inquirer.createPromptModule([]);
+
+appendAsync = async (path, data) => {
+	try {
+		await fs.appendFile(path, data, "utf-8");
+	} catch (err) {
+		throw err;
+	}
+};
+
+newLine = async (path) => {
+	await appendAsync(path, "\n");
+};
+
+inquirer
+	.prompt([
+		{
+			name: "title",
+			message: "What is the title of your project:",
+			type: "input",
+		},
+		{
+			name: "description",
+			message: "Describe your project:",
+			type: "input",
+		},
+	])
+	.then(async (answers) => {
+		const outputName = "README.md";
+		const outputDir = path.join(__dirname, "Output", outputName);
+
+		console.log(answers);
+
+		try {
+			await fs.writeFile(outputDir, "");
+		} catch (err) {
+			throw err;
+		}
+
+		await appendAsync(outputDir, `# ${answers.title}`);
+		await newLine(outputDir);
+		await appendAsync(outputDir, `## Description`);
+		await newLine(outputDir);
+		await appendAsync(outputDir, answer.description);
+	});
